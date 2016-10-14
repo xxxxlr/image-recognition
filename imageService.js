bbfdbd83eb21bb0fd18e212c5443a45f701cd407
Ran Li <Ran.Li@fmr.com>
2016-10-14 08:02:42 -0400

    Able to process hololens request, get response from google vision api, send response back to hololens

---
+++ imageService.js
@@ -0,0 +1,122 @@
+// Read the file into memory.
+var fs = require('fs');
+var gm = require('gm');
+var gcloud = require('google-cloud');
+var Vision = gcloud.vision;
+var vision = Vision();
+var _ = require('underscore');
+
+
+// var imgPath = './img/hololens.jpg';
+var resizedImgPath = './img/resize.png';
+var imgDir = './img';
+
+
+
+var ImgService = function(imgsReqOpts){
+    this.imgPath = imgsReqOpts ? imgsReqOpts.imgPath : '';
+    this.imgBase64 = imgsReqOpts ? imgsReqOpts.imgBase64 : '';
+    this.imgFile = imgsReqOpts ? imgsReqOpts.imgFile : '';
+}
+
+ImgService.prototype = _.extend(ImgService.prototype, {
+    reqRecogApi: function(callback){
+        if(this.imgPath){
+            this.imgFile = fs.readFileSync(this.imgPath);
+        } else {
+            console.error('Has to use a file path on local disk for now...');
+            return;
+        }
+        var imgPath = this.imgPath;
+        // checkImageFileSize(imgFile);
+        getSize(this.imgFile).then(function(value){
+            console.log(`Original: ${imgPath}:  width:${value.width} x height:${value.height}`)
+            return imgPath;
+        })
+        .then(function(imgPath){
+            return generateResizedImage(imgPath, './result/resize.png', 'LABLE_DETECTION');
+        })
+        .then(function(resizedImgPath){
+            return getSize(resizedImgPath)
+                .then(function(value){
+                    console.log(`Re-dimentioned: ${resizedImgPath}:  width:${value.width} x height:${value.height}`)
+                    return resizedImgPath;
+                })
+        })
+        .then(function(resizedImgPath){
+            detectLabels(resizedImgPath, callback);
+        })
+        .catch(function(error){
+            console.error(error);
+        })
+    }
+});
+
+
+/**
+ * Uses the Vision API to detect labels in the given file.
+ */
+function detectLabels (inputFile, callback) {
+    console.log(`Sending ${inputFile} to google vision API...`)
+    // Make a call to the Vision API to detect the labels
+    vision.detectLabels(inputFile, { verbose: true }, function (err, labels) {
+        if (err) {
+            return callback(err);
+        }
+        console.log('API results:', JSON.stringify(labels, null, 2));
+        console.log('Response to Hololens:', labels[0].desc);
+        callback(labels[0].desc);
+    });
+}
+//====================================================
+function getSize(imgPath){
+
+    return new Promise((resolve, reject) => {
+        gm(imgPath).size(function(err, value){
+            if(!err){
+                resolve(value);
+            } else {
+                reject(err);
+            }
+        })
+    });
+}
+function generateResizedImage(srcImgPath, targetImgPath, type){
+    // resize and remove EXIF profile data
+    // NOTE: image dimention recommendation: https://cloud.google.com/vision/docs/best-practices
+    IDEAL_IMAGE_DIM = {
+        LABLE_DETECTION: [640 , 480],
+        TEXT_DETECTION:[1024 , 768],
+        FACE_DETECTION:[1600 , 1200],
+        LANDMARK_DETECTION:[640 , 480],
+        LOGO_DETECTION:[640 , 480]
+    }
+    return new Promise((resolve, reject) => {
+        gm(srcImgPath)
+        .resize(...IDEAL_IMAGE_DIM[type])
+        .noProfile()
+        .write(targetImgPath, function (err) {
+            if (!err) {
+                resolve(targetImgPath);
+            } else {
+                reject(`write error when calling generateResizedImage(${srcImgPath}, ${targetImgPath})`);
+            }
+        });
+    })
+    
+}
+
+function checkImageFileSize(imgPath){
+    var stats = fs.statSync(imgPath)
+    var fileSizeInBytes = stats['size']
+    //Convert the file size to megabytes (optional)
+    var fileSizeInMegabytes = fileSizeInBytes / 1000000.0
+
+    if(fileSizeInMegabytes >= 4){
+        throw Error(`File size, ${fileSizeInMegabytes}MB is too big. Google vision API limit 4MB per image(https://cloud.google.com/vision/limits)`)
+    }
+
+    console.log(`Image size: ${fileSizeInMegabytes} MB`);
+}
+
+module.exports = ImgService;
---
+++ server.js
@@ -0,0 +1,147 @@
+var ImgService = require('./imageService');
+//Lets require/import the HTTP module
+var http = require('http');
+var fs = require('fs');
+//Lets define a port we want to listen to
+const PORT=8080;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    var requestTime = new Date();
+    console.log(`Request ${requestTime.getHours()}:${requestTime.getMinutes()}:${requestTime.getSeconds()} -- ${request.connection.remoteAddress}`);
+    //console.log(request);
+    response.writeHead(200, {'Content-Type': 'application/json'});
+
+
+    //send 'a' as binary in postman, with/without this encode, it's the same right result
+    //but for longer data that ascii can not represent, like image, it's messaged up without it.
+    //This tests receiver how to interpret body
+    //if the sender side used 'base64', then it should be that
+    request.setEncoding('binary');
+    var content = '';
+    request.on('data', function(chuck){
+        content += chuck;
+    })
+ 
+    request.on('error', console.error);
+    
+    request.on('end', function(req, res){
+        var outDir= './output/z';
+
+
+        console.log('base 64 raw content:');
+        console.log(content.slice(0,3));
+        
+        fs.writeFile(outDir + 'content.jpg', content, 'base64', function(err){
+            console.log('content error:', err);
+            console.log('Calling ImgService:');
+            var imgService = new ImgService({imgPath: outDir + 'content.jpg'});
+            imgService.reqRecogApi(function(result){
+                response.write(result);
+                response.end();
+            });
+        })
+
+        /*
+         
+        Examples for encode, buffer:
+        https://nodejs.org/api/buffer.html
+NOTE:
+content = '我';
+
+http://graphemica.com/%E6%88%91
+
+Encodings：
+    URL Escape Code	
+        %E6%88%91
+    UTF-8 (hex)	
+        0xE6 0x88 0x91 (e68891)
+    console.log:
+        <Buffer e6 88 91>
+        
+        */
+        
+
+/*
+NTOE:!!!
+var bufferedContent = new Buffer(content, 'base64');
+The second param tells buffer, what encoding state the string is at. so it can correctly decode it back to bits and bits.
+
+How file transfer work between client and server with encode and decode and save to local.
+e.g.
+    :client: ------------------------------------> network ------->------------------------- :server:
+string -> base64 encode it ->64ed_string  -------> network -------> recognized the bits stream(64ed_string) as 'base64'ed, and reverse it to get binary then save to fileName.
+
+you can use other encode, just let the receiving side understand how to interperate
+
+
+*/
+        var bufferedContent = new Buffer(content, 'binary');
+/*
+'abc'
+-> console.log                                                                                                               
+<Buffer 61 62 63> 
+Note: 
+    1.console.log print out byte by byte with space
+*/
+
+/*
+How do you know if a binary string is image? First few bytes:
+Some Extra info about other file format with jpeg: initial of file contains these bytes
+
+BMP : 42 4D
+JPG : FF D8 FF EO
+PNG : 89 50 4E 47
+GIF : 47 49 46 38
+
+when console.log(bufferedContent);
+- in buffered content:   
+=====                                                                                                           
+<Buffer 89 50 4e 47 .. .. .. ..>
+=====                                                                                                            
+
+- in raw content:    
+=====                                                                                                            
+PNG                                                                                                                        
+                                                                                                                            
+IHDR,y}ud IDATxíwÕ·ßS&(Ì(
+.....
+====
+*/
+      
+
+        console.log('buffered content:');
+        var len = bufferedContent.length;
+        console.log(bufferedContent.slice(0,4));
+        fs.writeFile(outDir + 'bufferedContent.jpg', bufferedContent, 'binary', function(err){
+            console.log('bufferedContent error:', err);
+        })       
+
+        var writeStream = fs.createWriteStream(outDir + 'stream.jpg');
+        writeStream.write(content);
+        writeStream.end();
+
+
+/*
+Good:
+    : no need to create a buffer to decode the base64, just write to file as 'baes64'!!!
+
+fs.writeFile("out.png", content, 'base64', function(err) {
+  console.log(err);
+});
+
+*/
+
+        // This pipes the POST data to the file
+        //req.pipe();
+    })
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
